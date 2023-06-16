525a883c6fdf48ddc6b563046a2ed866309422b3
julian <julianflesch@gmail.com>
2023-06-16 17:47:02 +0200

    experiments regularization and predictions

--- experiments/04_BaselineModel/whatTheFuckDoesRestructureDo.R
+++ experiments/04_BaselineModel/whatTheFuckDoesRestructureDo.R
@@ -1,8 +1,10 @@
 
-load("/home/julian/Uni/MasterThesis/psupplementary/data/acinar_sce.rda")
+load("/home/julian/Uni/MasterThesis/data/acinar_sce.rda")
 
 lambdas 	= 10^seq(from=0, to=-4, by=-0.1)
 xt <- t(SingleCellExperiment::logcounts(acinar_sce))
 y <- acinar_sce$donor_age
 
-newx <- psupertime:::.restructure_propodds(xt, y, rep(1, length(y)))
+# newx <- psupertime:::.restructure_propodds(xt, y, rep(1, length(y)))
+
+pobj <- psupertime::psupertime(x = acinar_sce, y=acinar_sce$donor_age)
